9bd14b4f33dbe93ae7e3deecf1aa1e6d3e8d8651
Muhammad-Shahid-Rasool <shahidrasool1529@gmail.com>
2023-02-03 01:27:03 +0500

    calculate-done

--- src/App.js
+++ src/App.js
@@ -6,28 +6,40 @@ import React, { useState } from 'react'
 
 const App = () => {
   const[result,setResult]=useState("");
+  
+  const clickHandler= (event)=> {
+    setResult(result.concat(event.target.value))
+}
+const clearDisplay=()=>{
+  setResult("");
+}
+
+const calcu=()=>{
+  setResult(eval(result))
+}
 
   return (
     <div className='calculator'>
-      <input type="text"  value="0" id='ans' value={result}/>
-      <input type="button" value="9" className='button'/>
-      <input type="button" value="8" className='button'/>
-      <input type="button" value="7"className='button'/>
-      <input type="button" value="6"className='button'/>
-      <input type="button" value="5"className='button'/>
-      <input type="button" value="4"className='button'/>
-      <input type="button" value="3"className='button'/>
-      <input type="button" value="2"className='button'/>
-      <input type="button" value="1"className='button'/>
-      <input type="button" value="0"className='button'/>
-      <input type="button" value="."className='button'/>
-      <input type="button" value="+"className='button'/>
-      <input type="button" value="-"className='button'/>
-      <input type="button" value="*"className='button'/>
-      <input type="button" value="/"className='button'/>
-      <input type="button" value="%"className='button'/>
-      <input type="button" value="clear"className='button button1'/>
-      <input type="button" value="="className='button button1'/>
+
+     <input type="text"  placeholder='0' id='ans' value={result}/>
+      <input type="button" value="9" className='button' onClick={clickHandler}/>
+      <input type="button" value="8" className='button'onClick={clickHandler}/>
+      <input type="button" value="7"className='button'onClick={clickHandler}/>
+      <input type="button" value="6"className='button'onClick={clickHandler}/>
+      <input type="button" value="5"className='button'onClick={clickHandler}/>
+      <input type="button" value="4"className='button'onClick={clickHandler}/>
+      <input type="button" value="3"className='button'onClick={clickHandler}/>
+      <input type="button" value="2"className='button'onClick={clickHandler}/>
+      <input type="button" value="1"className='button'onClick={clickHandler}/>
+      <input type="button" value="0"className='button'onClick={clickHandler}/>
+      <input type="button" value="."className='button'onClick={clickHandler}/>
+      <input type="button" value="+"className='button'onClick={clickHandler}/>
+      <input type="button" value="-"className='button'onClick={clickHandler}/>
+      <input type="button" value="*"className='button'onClick={clickHandler}/>
+      <input type="button" value="/"className='button'onClick={clickHandler}/>
+      <input type="button" value="%"className='button'onClick={clickHandler}/>
+      <input type="button" value="clear"className='button button1' onClick={clearDisplay}/>
+      <input type="button" value="="className='button button1' onClick={calcu}/>
      
 
 
