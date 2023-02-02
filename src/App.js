8aaac86b7d7c059bf379e329b13b15d6c9f4ddb1
Muhammad-Shahid-Rasool <shahidrasool1529@gmail.com>
2023-02-03 00:08:27 +0500

    add-css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+
+import React, { useState } from 'react'
+
+const App = () => {
+  const[result,setResult]=useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='calculator'>
+      <input type="text"  value="0" id='ans' value={result}/>
+      <input type="button" value="9" className='button'/>
+      <input type="button" value="8" className='button'/>
+      <input type="button" value="7"className='button'/>
+      <input type="button" value="6"className='button'/>
+      <input type="button" value="5"className='button'/>
+      <input type="button" value="4"className='button'/>
+      <input type="button" value="3"className='button'/>
+      <input type="button" value="2"className='button'/>
+      <input type="button" value="1"className='button'/>
+      <input type="button" value="0"className='button'/>
+      <input type="button" value="."className='button'/>
+      <input type="button" value="+"className='button'/>
+      <input type="button" value="-"className='button'/>
+      <input type="button" value="*"className='button'/>
+      <input type="button" value="/"className='button'/>
+      <input type="button" value="%"className='button'/>
+      <input type="button" value="clear"className='button button1'/>
+      <input type="button" value="="className='button button1'/>
+     
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
